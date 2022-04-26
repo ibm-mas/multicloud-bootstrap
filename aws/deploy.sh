8ba84235c128c801cb587a457c0d4f66d0d6d4a1
Vaibhav Kulkarni <Vaibhav.Kulkarni1@ibm.com>
2022-04-26 07:20:56 +0530

    annotation changes

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -292,6 +292,32 @@ fi
 
 ## Deploy MAS
 log "==== MAS deployment started ===="
+## Evalute custom annotations to set with reference from aws-product-codes.config
+# product_code_metadata="$(curl http://169.254.169.254/latest/meta-data/product-codes)"
+set -x
+product_code_metadata="vrxqov1ml7fjwasbi7pkw4m3"
+if [[ -n "$product_code_metadata" ]];then
+  log "Product Code: $product_code_metadata"
+  aws_product_codes_config_file="$GIT_REPO_HOME/aws/aws-product-codes.config"
+  log "Checking for product type corrosponding to $product_code_metadata from file $aws_product_codes_config_file"
+  if grep -E "^$product_code_metadata:" $aws_product_codes_config_file 1>/dev/null 2>&1;then
+    product_type="$(grep -E "^$product_code_metadata:" $aws_product_codes_config_file | cut -f 3 -d ":")"
+    if [[ $product_type == "byol" ]];then
+      export MAS_ANNOTATIONS="mas.ibm.com/hyperscalerProvider=aws,mas.ibm.com/hyperscalerFormat=byol,mas.ibm.com/hyperscalerChannel=ibm"
+    elif [[ $product_type == "privatepublic" ]];then
+      export MAS_ANNOTATIONS="mas.ibm.com/hyperscalerProvider=aws,mas.ibm.com/hyperscalerFormat=privatepublic,mas.ibm.com/hyperscalerChannel=aws"
+    else
+      log "Invalid product type : $product_type"
+      exit 28
+    fi
+  else
+    log "Product code not found in file $aws_product_codes_config_file"
+    exit 28
+  fi
+else
+  log "MAS product code not found, skipping custom annotations suite_install"
+fi
+set +x
 ansible-playbook mas/install-suite.yml
 log "==== MAS deployment completed ===="
 
