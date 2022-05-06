f9b95d48ccefa6e97fc8d954139a2bccf5357ef6
Vaibhav Kulkarni <vaibhav.kulkarni1@ibm.com>
2022-05-06 15:33:14 +0530

    Updates to role names

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -163,13 +163,14 @@ cp $GIT_REPO_HOME/entitlement.lic $MAS_CONFIG_DIR
 if [[ (-z $SLS_ENDPOINT_URL) || (-z $SLS_REGISTRATION_KEY) || (-z $SLS_PUB_CERT_URL) ]]; then
   ## Deploy SLS
   log "==== SLS deployment started ===="
+  # sls and gencfg_sls are combined in common sls role
   export ROLE_NAME=sls && ansible-playbook ibm.mas_devops.run_role
-  export ROLE_NAME=gencfg_sls && ansible-playbook ibm.mas_devops.run_role
   log "==== SLS deployment completed ===="
 
 else
-  log "=== Using Existing SLS Deployment ==="
-  export ROLE_NAME=gencfg_sls && ansible-playbook ibm.mas_devops.run_role
+  log "=== Using Existing SLS Deployment ==="  #
+  # works when SLSCFG_URL is set, handled in same sls role
+  export ROLE_NAME=sls && ansible-playbook ibm.mas_devops.run_role
   log "=== Generated SLS Config YAML ==="
 fi
 
@@ -177,13 +178,14 @@ fi
 if [[ (-z $UDS_API_KEY) || (-z $UDS_ENDPOINT_URL) || (-z $UDS_PUB_CERT_URL) ]]; then
   # Deploy UDS
   log "==== UDS deployment started ===="
-  export ROLE_NAME=uds_install && ansible-playbook ibm.mas_devops.run_role
-  export ROLE_NAME=gencfg_uds && ansible-playbook ibm.mas_devops.run_role
+  # uds and gencfg_uds are combined in common uds role
+  export ROLE_NAME=uds && ansible-playbook ibm.mas_devops.run_role
   log "==== UDS deployment completed ===="
 
 else
   log "=== Using Existing UDS Deployment ==="
-  export ROLE_NAME=gencfg_uds && ansible-playbook ibm.mas_devops.run_role
+  # works when UDS_ENDPOINT_URL is set, handled in same uds role
+  export ROLE_NAME=uds && ansible-playbook ibm.mas_devops.run_role
   log "=== Generated UDS Config YAML ==="
 fi
 
@@ -192,7 +194,7 @@ if [[ $DEPLOY_CP4D == "true" ]]; then
   log "==== CP4D deployment started ===="
   export ROLE_NAME=cp4d_install && ansible-playbook ibm.mas_devops.run_role
   export ROLE_NAME=cp4d_install_services && ansible-playbook ibm.mas_devops.run_role
-  export ROLE_NAME=cp4d_db2wh && ansible-playbook ibm.mas_devops.run_role
+  export ROLE_NAME=db2 && ansible-playbook ibm.mas_devops.run_role
   log "==== CP4D deployment completed ===="
 fi
 
@@ -224,6 +226,6 @@ if [[ $DEPLOY_MANAGE == "true" ]]; then
 
   # Configure app to use the DB
   log "==== MAS Manage configure app started ===="
-  export ROLE_NAME=suite_app_configure && ansible-playbook ibm.mas_devops.run_role
+  export ROLE_NAME=suite_app_config && ansible-playbook ibm.mas_devops.run_role
   log "==== MAS Manage configure app completed ===="
 fi
