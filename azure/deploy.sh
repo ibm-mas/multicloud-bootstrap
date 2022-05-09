7b2cf58a946761ea3d42d2d15af274c84bb1188e
vishwajitdandage-psl <vishwajit_dandage@persistent.com>
2022-05-09 01:04:14 -0500

    updated role names

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -124,9 +124,10 @@ fi
 log "==== OCP cluster configuration (Cert Manager and SBO) started ===="
 cd $GIT_REPO_HOME/../ibm/mas_devops/playbooks
 set +e
-export ROLE_NAME=ocp_setup_ibm_catalogs && ansible-playbook ibm.mas_devops.run_role
-export ROLE_NAME=ocp_setup_common_services && ansible-playbook ibm.mas_devops.run_role
-export ROLE_NAME=ocp_setup_mas_deps && ansible-playbook ibm.mas_devops.run_role
+export ROLE_NAME=ibm_catalogs && ansible-playbook ibm.mas_devops.run_role
+export ROLE_NAME=common_services && ansible-playbook ibm.mas_devops.run_role
+export ROLE_NAME=cert_manager && ansible-playbook ibm.mas_devops.run_role
+export ROLE_NAME=sbo && ansible-playbook ibm.mas_devops.run_role
 if [[ $? -ne 0 ]]; then
   # One reason for this failure is catalog sources not having required state information, so recreate the catalog-operator pod
   # https://bugzilla.redhat.com/show_bug.cgi?id=1807128
@@ -136,7 +137,10 @@ if [[ $? -ne 0 ]]; then
   oc delete pod $podname -n openshift-operator-lifecycle-manager
   sleep 10
   # Retry the step
-  export ROLE_NAME=ocp_setup_mas_deps && ansible-playbook ibm.mas_devops.run_role
+  export ROLE_NAME=ibm_catalogs && ansible-playbook ibm.mas_devops.run_role
+  export ROLE_NAME=common_services && ansible-playbook ibm.mas_devops.run_role
+  export ROLE_NAME=cert_manager && ansible-playbook ibm.mas_devops.run_role
+  export ROLE_NAME=sbo && ansible-playbook ibm.mas_devops.run_role
   retcode=$?
   if [[ $retcode -ne 0 ]]; then
     log "Failed while configuring OCP cluster"
