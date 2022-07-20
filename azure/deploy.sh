877e79971b1c0cf8a7dc6f1b8a55b25e24971fe5
vskhabani <v.s.khabani@gmail.com>
2022-07-20 14:23:52 +0530

    upi script code addition

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -85,7 +85,7 @@ if [[ $OPENSHIFT_USER_PROVIDE == "false" ]]; then
     set -e
   fi
 
-  exit 0
+  
   oc login -u $OCP_USERNAME -p $OCP_PASSWORD --server=https://api.${CLUSTER_NAME}.${BASE_DOMAIN}:6443
   log "==== Adding PID limits to worker nodes ===="
   oc create -f $GIT_REPO_HOME/templates/container-runtime-config.yml
--- azure/upifiles/create-ocp-cluster-upi.sh
+++ azure/upifiles/create-ocp-cluster-upi.sh
@@ -8,6 +8,7 @@ python3 -m pip install yq
 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.8.11/openshift-install-linux-4.8.11.tar.gz
 tar xzvf openshift-install-linux-4.8.11.tar.gz
 mv openshift-install /usr/local/bin/
+rm -rf ./openshift-install-linux-4.8.11.tar.gz
 #these params can be passed from ARM templates to the script 
 log "===== Setting Environment Variables ====="
 
@@ -27,7 +28,7 @@ export INFRA_ID=($(echo $vnetName | tr '-' "\n"))
 export clientID=${AZURE_SP_CLIENT_ID}
 export clientSecret=${AZURE_SP_CLIENT_PWD}
 export tenantId=${TENANT_ID}
-
+export SA_NAME="masocp${RANDOM_STR}sa"
 #Login to the azure  account
 az login --service-principal -u ${clientID} -p ${clientSecret} --tenant ${tenantId}
 # az login --identity
@@ -46,3 +47,88 @@ log "===== Creation of osServicePrincipal.json Completed ====="
 log "===== Creation of Install-Config.yaml Started ====="
 envsubst < install-config-template.yaml | tee install-config.yaml
 log "===== Creation of Install-Config.yaml Completed ====="
+
+#Set worker node count to 0 
+python3 -c '
+import yaml;
+path = "install-config.yaml";
+data = yaml.full_load(open(path));
+data["compute"][0]["replicas"] = 0;
+open(path, "w").write(yaml.dump(data, default_flow_style=False))'
+
+#create manifest file (This will consume the install-config.yaml file created above )
+log "===== Creating manifests files ====="
+openshift-install create manifests
+if [ $? -ne 0 ]; then
+    log "ERROR: Unable to create manifests "
+    exit 1
+fi
+log "===== Manifests files created successfully  ====="
+
+#Removing unwanted files 
+rm -f openshift/99_openshift-cluster-api_master-machines-*.yaml
+rm -f openshift/99_openshift-cluster-api_worker-machineset-*.yaml
+
+python3 -c '
+import yaml;
+path = "manifests/cluster-scheduler-02-config.yml";
+data = yaml.full_load(open(path));
+data["spec"]["mastersSchedulable"] = False;
+open(path, "w").write(yaml.dump(data, default_flow_style=False))'
+
+python3 -c '
+import yaml;
+path = "manifests/cluster-dns-02-config.yml";
+data = yaml.full_load(open(path));
+del data["spec"]["publicZone"];
+del data["spec"]["privateZone"];
+open(path, "w").write(yaml.dump(data, default_flow_style=False))'
+
+#Setup manifest files as per the Infra id and Resource group name 
+log "===== Setting Resource group and Infra id values in all the manifests file ====="
+python3 setup-manifests.py $RESOURCE_GROUP $INFRA_ID
+
+#Create Ignition config files (This step will consume all the manifests files)
+log "===== Creating Ignition config files ====="
+openshift-install create ignition-configs
+if [ $? -ne 0 ]; then
+    log "ERROR: Unable to create ignition configs "
+    exit 1
+fi
+log "===== Ignition config files created successfully ====="
+
+#create managed Identity for the 
+log "===== Creating managed identity  ====="
+az identity create -g $RESOURCE_GROUP -n ${INFRA_ID}-identity
+if [ $? -ne 0 ]; then
+    log "ERROR: Unable to create Managed identity .. Please check the permissions"
+    exit 1
+fi
+log "===== Managed identity created successfully ====="
+
+#Create storage account and start copying vhd file
+log "===== Creating Storage account and starting VHD file copying started  ====="
+az storage account create -g $RESOURCE_GROUP --location $AZURE_REGION --name $SA_NAME --kind Storage --sku Standard_LRS
+if [ $? -ne 0 ]; then
+    log "ERROR: Unable to Storage account"
+    exit 1
+fi
+export ACCOUNT_KEY=`az storage account keys list -g $RESOURCE_GROUP --account-name $SA_NAME --query "[0].value" -o tsv`
+
+az storage container create --name vhd --account-name $SA_NAME
+export VHD_URL=$(openshift-install coreos print-stream-json | jq -r '.architectures.x86_64."rhel-coreos-extensions"."azure-disk".url')
+az storage blob copy start --account-name $SA_NAME --account-key $ACCOUNT_KEY --destination-blob "rhcos.vhd" --destination-container vhd --source-uri "$VHD_URL"
+if [ $? -ne 0 ]; then
+    log "ERROR: Unable to start VHD copy operation "
+    exit 1
+fi
+#wait until the VHD file gets copied 
+log "===== Wait untill VHD image is copied ====="
+status="unknown"
+while [ "$status" != "success" ]
+do
+  status=`az storage blob show --container-name vhd --name "rhcos.vhd" --account-name $SA_NAME --account-key $ACCOUNT_KEY -o tsv --query properties.copy.status`
+  sleep 10
+  echo $status
+done
+log "===== VHD image copied successfully ====="
