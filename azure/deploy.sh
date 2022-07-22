7bf403df914efe4c3d6df16eb5f5e04fd51d1cf7
vskhabani <v.s.khabani@gmail.com>
2022-07-22 11:00:00 +0530

    oc login command change

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -85,8 +85,8 @@ if [[ $OPENSHIFT_USER_PROVIDE == "false" ]]; then
     set -e
   fi
 
-  echo "oc login -u $OCP_USERNAME -p $OCP_PASSWORD --server=https://api.${CLUSTER_NAME}.${BASE_DOMAIN}:6443 --insecure-skip-tls-verify=true"
-  oc login -u $OCP_USERNAME -p $OCP_PASSWORD --server=https://api.${CLUSTER_NAME}.${BASE_DOMAIN}:6443 --insecure-skip-tls-verify=true
+  export OCP_SERVER="$(echo https://api.${CLUSTER_NAME}.${BASE_DOMAIN}:6443)"
+  oc login -u $OCP_USERNAME -p $OCP_PASSWORD --server=$OCP_SERVER --insecure-skip-tls-verify=true
   log "==== Adding PID limits to worker nodes ===="
   oc create -f $GIT_REPO_HOME/templates/container-runtime-config.yml
 
