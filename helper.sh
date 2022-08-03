8b5ef8daf14c89d110474d7428283f9239569c14
Vishwajit Dandage <83405861+vishwajitdandage-psl@users.noreply.github.com>
2022-08-03 15:54:55 +0530

    updated output message

--- helper.sh
+++ helper.sh
@@ -124,7 +124,7 @@ mark_provisioning_failed() {
   elif [[ $retcode -eq 28 ]]; then
     export STATUS_MSG="Missing or Invalid Product Code."
   elif [[ $retcode -eq 29 ]]; then
-    export STATUS_MSG="Provided cluster does not meet the requirements. Please select option to create a new cluster in a new deployment."
+    export STATUS_MSG="Provided cluster does not meet the requirements. Please select option to create a new cluster in a new deployment. (Please check provisioning logs for more details)"
   elif [[ $retcode -eq 30 ]]; then
     export STATUS_MSG="MAS+CP4D offering is not supported on ROSA cluster. Please select option to create a new cluster in a new deployment or provide a self-managed cluster."
   fi
