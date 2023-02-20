0623c94f48522f4c0a58d8edbc6c8a786a969b8f
shajeena <shajsyed@in.ibm.com>
2023-02-20 16:24:40 +0530

    changes

--- azure/create-ocp-cluster.sh
+++ azure/create-ocp-cluster.sh
@@ -29,8 +29,6 @@ virtual-network-name    = "$EXISTING_NETWORK"
 new-or-existing         = "$new_or_existing_vpc_subnet"
 existing-vnet-resource-group = "$EXISTING_NETWORK_RG"
 private-or-public-cluster = "$private_or_public_cluster"
-master-subnet-name        =  "$master_subnet_name"
-worker-subnet-name        =  "$worker_subnet_name"
 EOT
 
 if [[ -f terraform.tfvars ]]; then
