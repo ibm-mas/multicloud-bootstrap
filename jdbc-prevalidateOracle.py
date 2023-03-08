b006b5eeadfc8902b63aaab60e9107c353282e32
033982744 <033982744@LAPTOP-69F31GB6>
2023-03-08 18:53:58 +0530

    changes..

--- jdbc-prevalidateOracle.py
+++ jdbc-prevalidateOracle.py
@@ -1,11 +1,9 @@
 import jaydebeapi
 import os
 con=jaydebeapi.connect('oracle.jdbc.driver.OracleDriver',
-                        os.getenv('MAS_JDBC_URL').replace(':sslConnection=true;',''),
+                        os.getenv('MAS_JDBC_URL'),
                         {'user':os.getenv('MAS_JDBC_USER'),
-                        'password':os.getenv('MAS_JDBC_PASSWORD'),
-                        'sslConnection':'true',
-                        'sslCertLocation':os.getenv('MAS_JDBC_CERT_LOCAL_FILE')},
+                        'password':os.getenv('MAS_JDBC_PASSWORD')},
                         jars=os.getenv('MAS_ORACLE_JAR_LOCAL_PATH'),)
 
 if con :
