848e7e5cd1e0cb669acbddef840ce2bdcf883ee6
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2023-03-24 21:10:01 +0530

    Update deploy.sh

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -26,6 +26,7 @@ export CPD_METADATA_STORAGE_CLASS=gp2
 export CPD_SERVICE_STORAGE_CLASS="ocs-storagecluster-cephfs"
 export SSL_ENABLED=false
 export MAS_APPWS_BINDINGS_JDBC="workspace-application"
+export MAS_DB_IMPORT_DEMO_DATA=true
 
 # Retrieve SSH public key
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
