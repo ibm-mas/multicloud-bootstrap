7658670fd9d12de317d748bcec6ad564cf2a0c37
033982744 <033982744@LAPTOP-69F31GB6>
2023-03-31 17:13:40 +0530

    changes into my branch..

--- init.sh
+++ init.sh
@@ -244,7 +244,7 @@ export MAS_APP_ID=manage
 export MAS_APPWS_JDBC_BINDING="workspace-application"
 export MAS_JDBC_CERT_LOCAL_FILE=$GIT_REPO_HOME/db.crt
 export MAS_CLOUD_AUTOMATION_VERSION=1.0
-export MAS_DEVOPS_COLLECTION_VERSION=12.11.1
+export MAS_DEVOPS_COLLECTION_VERSION=12.16.1
 export MAS_APP_CHANNEL=8.6.x
 if [ -z "$EXISTING_NETWORK" ]; then
   export new_or_existing_vpc_subnet="new"
