ee5d4ba10633f2a9ef00c719cc492d49593f02c1
shajeena <shajsyed@in.ibm.com>
2023-05-25 14:26:18 +0530

    810-rhel8

--- init.sh
+++ init.sh
@@ -212,9 +212,9 @@ export UDS_TLS_CERT_LOCAL_FILE_PATH="${GIT_REPO_HOME}/uds.crt"
 # CP4D variables
 export CPD_ENTITLEMENT_KEY=$SLS_ENTITLEMENT_KEY
 export CPD_VERSION=cpd40
-export CPD_PRODUCT_VERSION=4.6.0
+export CPD_PRODUCT_VERSION=4.6.3
 export MAS_CHANNEL=8.10.x
-export MAS_CATALOG_VERSION=v8-230328-amd64
+export MAS_CATALOG_VERSION=v8-230518-amd64
 if [[ $CLUSTER_TYPE == "aws" ]]; then
   export CPD_PRIMARY_STORAGE_CLASS="ocs-storagecluster-cephfs"
 elif [[ $CLUSTER_TYPE == "azure" ]]; then
@@ -244,7 +244,7 @@ export MAS_APP_ID=manage
 export MAS_APPWS_JDBC_BINDING="workspace-application"
 export MAS_JDBC_CERT_LOCAL_FILE=$GIT_REPO_HOME/db.crt
 export MAS_CLOUD_AUTOMATION_VERSION=1.0
-export MAS_DEVOPS_COLLECTION_VERSION=13.0.0
+export MAS_DEVOPS_COLLECTION_VERSION=13.8.2
 export MAS_APP_CHANNEL=8.6.x
 if [ -z "$EXISTING_NETWORK" ]; then
   export new_or_existing_vpc_subnet="new"
