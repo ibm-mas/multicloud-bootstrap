52cf1da13ffc43dad0821519b8691a56071d5bc2
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2023-07-28 19:27:50 +0530

    Update pre-validate.sh

--- pre-validate.sh
+++ pre-validate.sh
@@ -86,7 +86,7 @@ fi
 #    SCRIPT_STATUS=25
 #fi
 
-if [[ -n $DB2ProvisionedVPCId ]]; then
+if [[ -n $DBProvisionedVPCId ]]; then
 cd $GIT_REPO_HOME
 log "==== Invoke db-create-vpc-peer.sh to create VPC Peering between bootnode VPC & database provisioned VPC ID starts ===="
 log "==== This VPC peering is done to pre-validate database connection ==="
