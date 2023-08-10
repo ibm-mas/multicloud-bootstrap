30c2a3a36809a27e931823e883d07dbd9b19d678
shajeena <shajsyed@in.ibm.com>
2023-08-10 07:42:58 +0530

    cidr

--- azure/ocp-terraform/azure_infra/data.tf
+++ azure/ocp-terraform/azure_infra/data.tf
@@ -1,5 +1,6 @@
 locals {
   cidr-prefix = split(".", var.virtual-network-cidr)[0]
+  cidr-prefix2 = split(".", var.virtual-network-cidr)[1]
 }
 
 data "template_file" "azurecreds" {
@@ -28,7 +29,7 @@ data "template_file" "installconfig" {
     computeSubnet               = var.worker-subnet-name
     networkResourceGroupName    = local.resource-group
 
-    cluster-network-cidr = "${local.cidr-prefix}.128.0.0/14"
+    cluster-network-cidr = "${local.cidr-prefix}.${local.cidr-prefix2}.0.0/14"
     host-prefix          = 23
     virtual-network-cidr = var.virtual-network-cidr
     service-network-cidr = "192.30.0.0/16"
