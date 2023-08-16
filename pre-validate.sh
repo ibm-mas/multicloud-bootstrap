958ac4165fa8c2df1c97067a98b8f241b668f1b6
shajeena <shajsyed@in.ibm.com>
2023-08-16 18:45:27 +0530

    subscription

--- pre-validate.sh
+++ pre-validate.sh
@@ -310,7 +310,7 @@ if [[ $CLUSTER_TYPE == "azure" ]]; then
 fi
 #Validate the subscriptionId
 if [[ $CLUSTER_TYPE == "azure" ]]; then
-  export AZURE_VALIDATE_SUBSC_ID=az account list --query "[?id == '$SELLER_SUBSCRIPTION_ID'].{Id:id}" -o tsv
+  export AZURE_VALIDATE_SUBSC_ID=`az account list --query "[?id == '$SELLER_SUBSCRIPTION_ID'].{Id:id}" -o tsv`
   if [[ -n $AZURE_VALIDATE_SUBSC_ID ]]; then
     export AZURE_SUBSC_ID=AZURE_VALIDATE_SUBSC_ID
   else
