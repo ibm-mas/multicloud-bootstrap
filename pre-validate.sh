73bd42d233f6abe5c0cac783a8d5e0c10f7599b9
shajeena <shajsyed@in.ibm.com>
2023-08-16 21:45:33 +0530

    subscription

--- pre-validate.sh
+++ pre-validate.sh
@@ -314,7 +314,7 @@ if [[ $CLUSTER_TYPE == "azure" ]]; then
    az login --service-principal -u ${AZURE_SP_CLIENT_ID} -p ${AZURE_SP_CLIENT_PWD} --tenant ${TENANT_ID}
   export AZURE_VALIDATE_SUBSC_ID=`az account list --query "[?id == '$SELLER_SUBSCRIPTION_ID'].{Id:id}" -o tsv`
   if [[ -n $AZURE_VALIDATE_SUBSC_ID ]]; then
-    export AZURE_SUBSC_ID=AZURE_VALIDATE_SUBSC_ID
+    export AZURE_SUBSC_ID=$AZURE_VALIDATE_SUBSC_ID
      log " AZURE_SUBSC_ID: $AZURE_SUBSC_ID"
   else
     log "ERROR: Subscription Id Invalid"
