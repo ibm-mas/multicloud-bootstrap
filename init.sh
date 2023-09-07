4de251bd8898ce712835e50431a796d3f7c48f0f
shajeena <shajsyed@in.ibm.com>
2023-09-08 00:03:48 +0530

    vnetpeering

--- init.sh
+++ init.sh
@@ -66,7 +66,9 @@ export MONGO_HOSTS=${57}
 export MONGO_CA_PEM_FILE=${58}
 export DOCUMENTDB_VPC_ID=${59}
 export AWS_MSK_PROVIDER=${60}
-export ENV_TYPE=${61}
+export DBProvisionedVPCId=${61}
+export ExocpProvisionedVPCId=${62}
+export ENV_TYPE=${63}
 export GIT_REPO_HOME=$(pwd)
 # Load helper functions
 . helper.sh
@@ -269,6 +271,7 @@ if [[ -z "$EXISTING_NETWORK" && $CLUSTER_TYPE == "azure" ]]; then
   export INSTALLATION_MODE="IPI"
   # Setting the name of the v-net
   export EXISTING_NETWORK=${RANDOM_STR}-vnet
+  export ExocpProvisionedVPCId=$EXISTING_NETWORK
 else
   export INSTALLATION_MODE="UPI"
 fi
@@ -418,68 +421,6 @@ else
   PRE_VALIDATION=pass
 fi
 log "===== PRE-VALIDATION COMPLETED ($PRE_VALIDATION) ====="
-
-if [[ $CLUSTER_TYPE == "azure" ]]; then
-  # Perform az login
-  az login --service-principal -u ${AZURE_SP_CLIENT_ID} -p ${AZURE_SP_CLIENT_PWD} --tenant ${TENANT_ID}
-  az resource list -n masocp-${RANDOM_STR}-bootnode-vm
-
-  # Get subscription ID
-
- # export AZURE_SUBSC_ID=`az account list | jq -r '.[].id'`
- export AZURE_SUBSC_ID=`az account list --query "[?id == '$SELLER_SUBSCRIPTION_ID'].{Id:id}" -o tsv`
-  log " AZURE_SUBSC_ID: $AZURE_SUBSC_ID"
-  # Get Base domain RG name
-  DNS_ZONE=$BASE_DOMAIN
-  export BASE_DOMAIN_RG_NAME=`az network dns zone list | jq --arg DNS_ZONE $DNS_ZONE '.[] | select(.name==$DNS_ZONE).resourceGroup' | tr -d '"'`
-  log " BASE_DOMAIN_RG_NAME: $BASE_DOMAIN_RG_NAME"
-  # Get VNet RG name for UPI based installation
-  if [[ $INSTALLATION_MODE == "UPI" ]]; then
-    # Domain name with private dns - only available for UPI
-    if [[ $PRIVATE_CLUSTER == "true" ]]; then
-        export private_or_public_cluster="private"
-        export BASE_DOMAIN_RG_NAME=`az network private-dns zone list | jq --arg DNS_ZONE $DNS_ZONE '.[] | select(.name==$DNS_ZONE).resourceGroup' | tr -d '"'`
-         log " UPI PRIVATE CLUSTER - BASE_DOMAIN_RG_NAME: $BASE_DOMAIN_RG_NAME"
-      else
-         export private_or_public_cluster="public"
-         export BASE_DOMAIN_RG_NAME=`az network dns zone list | jq --arg DNS_ZONE $DNS_ZONE '.[] | select(.name==$DNS_ZONE).resourceGroup' | tr -d '"'`
-         log " UPI PUBLIC CLUSTER - BASE_DOMAIN_RG_NAME: $BASE_DOMAIN_RG_NAME"
-      fi
-
-       VNET_NAME=$EXISTING_NETWORK
-       export EXISTING_NETWORK_RG=`az network vnet list | jq --arg VNET_NAME $VNET_NAME '.[] | select(.name==$VNET_NAME).resourceGroup' | tr -d '"'`
-        #Assign the nsg name
-      # export nsg_name=`az network vnet subnet list --resource-group $EXISTING_NETWORK_RG --vnet-name  $VNET_NAME|jq '.[0] | select(.name).networkSecurityGroup.id'|awk -F'/' '{print $9}'|tr -d '"'`
-        #Assign the network subnet
-       export  master_subnet_name=`az network vnet subnet list --resource-group $EXISTING_NETWORK_RG --vnet-name $VNET_NAME|jq '.[] | select(.name).name'|grep master|tr -d '"'`
-       export  worker_subnet_name=`az network vnet subnet list --resource-group $EXISTING_NETWORK_RG --vnet-name $VNET_NAME|jq '.[] | select(.name).name'|grep worker|tr -d '"'`
-       export  virtual_network_cidr=`az network vnet show --resource-group $EXISTING_NETWORK_RG -n $VNET_NAME|jq -r '.addressSpace.addressPrefixes[0]'|tr -d '"'`
-       export  master_subnet_cidr=`az network vnet subnet show --resource-group $EXISTING_NETWORK_RG --vnet-name $VNET_NAME -n master-subnet|jq  -r '.addressPrefix'`
-       export  worker_subnet_cidr=`az network vnet subnet show --resource-group $EXISTING_NETWORK_RG --vnet-name $VNET_NAME -n worker-subnet|jq  -r '.addressPrefix'`
-       Ip_range=$worker_subnet_cidr
-       #10.0.3.224/27
-       export bastion_cidr=`echo $Ip_range|cut -d "." -f 1`.`echo $Ip_range|cut -d "." -f 2`.3.224/27
-  elif [[ $INSTALLATION_MODE == "IPI" ]]; then
-    # Setting the cidr ranges for IPI mode
-      export  master_subnet_name="master-subnet"
-      export  worker_subnet_name="worker-subnet"
-      export  virtual_network_cidr="10.0.0.0/16"
-      export  master_subnet_cidr="10.0.1.0/24"
-      export  worker_subnet_cidr="10.0.2.0/24"
-      export bastion_cidr="10.0.3.224/27"
-
-  fi
-        log " MASTER SUBNET NAME: $master_subnet_name "
-           log " WORKER SUBNET NAME: $worker_subnet_name"
-           log " VNET CIDR RANGE: $virtual_network_cidr "
-           log " MASTER SUBNET CIDR RANGE: $master_subnet_cidr "
-           log " WORKER SUBNET CIDR RANGE : $worker_subnet_cidr"
-           log " BASTION  CIDR RANGE : $bastion_cidr"
-           log " cluster_network_cidr : $cluster_network_cidr"
-        #  log " NSG NAME: $nsg_name"
-          log " EXISTING_NETWORK_RG: $EXISTING_NETWORK_RG"
-fi
-
 
 
 # Perform the MAS deployment only if pre-validation checks are passed
