515660201c04995e0fb85feefd079e9b99eb27b5
033982744 <033982744@LAPTOP-69F31GB6>
2023-09-08 04:52:01 +0530

    changes..

--- aws/ocp-terraform/ocp/variables.tf
+++ aws/ocp-terraform/ocp/variables.tf
@@ -39,7 +39,7 @@ variable "worker_instance_type" {
 
 variable "worker_instance_volume_iops" {
   type    = number
-  default = 2000
+  default = 900
 }
 
 variable "worker_instance_volume_size" {
@@ -64,12 +64,12 @@ variable "master_instance_type" {
 
 variable "master_instance_volume_iops" {
   type    = number
-  default = 4000
+  default = 1000
 }
 
 variable "master_instance_volume_size" {
   type    = number
-  default = 300
+  default = 350
 }
 
 variable "master_instance_volume_type" {
