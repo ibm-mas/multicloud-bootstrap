77fd39003100f75471ecf68c8734ac2fd918ac71
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2023-09-19 04:58:48 +0530

    Update pre-validate.sh

--- pre-validate.sh
+++ pre-validate.sh
@@ -139,7 +139,7 @@ if [[ $DEPLOY_MANAGE == "true" ]]; then
                     SCRIPT_STATUS=14
                 fi
             elif [[ ${MAS_JDBC_URL,, } =~ ^jdbc:oracle? ]]; then
-                export MAS_ORACLE_JAR_LOCAL_PATH=$GIT_REPO_HOME/lib/ojdbc8.jar
+                export MAS_ORACLE_JAR_LOCAL_PATH=$GIT_REPO_HOME/lib/oraclethin.jar
                 log "Connecting to Oracle Database"
                 if python jdbc-prevalidateOracle.py; then
                     log "Oracle JDBC URL Validation = PASS"
