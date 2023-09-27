e2c99df49c41f3a704b0f57279a47d67a6ed734e
aadawadk <114399757+aadawadk@users.noreply.github.com>
2023-09-27 10:55:49 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -218,7 +218,7 @@ export CPD_ENTITLEMENT_KEY=$SLS_ENTITLEMENT_KEY
 export CPD_VERSION=cpd40
 export CPD_PRODUCT_VERSION=4.6.3
 export MAS_CHANNEL=8.11.x
-export MAS_CATALOG_VERSION=v8-230829-amd64
+export MAS_CATALOG_VERSION=v8-230926-amd64
 if [[ $CLUSTER_TYPE == "aws" ]]; then
   export CPD_PRIMARY_STORAGE_CLASS="ocs-storagecluster-cephfs"
 elif [[ $CLUSTER_TYPE == "azure" ]]; then
