f272da1268207d84b224b354b296005b0eaca85e
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2023-10-27 04:47:51 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -256,7 +256,7 @@ export MAS_APP_BINDINGS_JDBC="workspace-application"
 export MAS_JDBC_CERT_LOCAL_FILE=$GIT_REPO_HOME/db.crt
 export MAS_CLOUD_AUTOMATION_VERSION=1.0
 export MAS_DEVOPS_COLLECTION_VERSION=18.3.4
-export MAS_APP_CHANNEL=8.7.x
+export MAS_APP_CHANNEL=8.6.x
 if [ -z "$EXISTING_NETWORK" ]; then
   export new_or_existing_vpc_subnet="new"
   export enable_permission_quota_check=true
