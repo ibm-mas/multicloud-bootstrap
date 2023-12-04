afc9a79da4466e59e9cf81930339489910db18a9
shajeena <shajsyed@in.ibm.com>
2023-12-04 16:42:52 +0530

    Azure bootnode fix and CUA

--- azure/ocp-terraform/azure_infra/provider.tf
+++ azure/ocp-terraform/azure_infra/provider.tf
@@ -7,3 +7,9 @@ provider "azurerm" {
   partner_id      = "pid-5a1e8d63-79c9-418d-b3af-b2a21a557aac-partnercenter"
   features {}
 }
+  required_providers {
+  azurerm = {
+  source  = "hashicorp/azurerm"
+  version = "3.21.0"
+  }
+  }
