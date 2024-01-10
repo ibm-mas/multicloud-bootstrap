22ed070df156d842f8da21adc20e6d296021f011
shajeena <shajsyed@in.ibm.com>
2024-01-10 10:54:37 +0530

    ARO Changes

--- azure/azurefiles/azurefiles-premium.sh
+++ azure/azurefiles/azurefiles-premium.sh
@@ -20,6 +20,10 @@ oc adm policy add-scc-to-user privileged system:serviceaccount:kube-system:csi-a
 #Install CSI Driver
 oc create configmap azure-cred-file --from-literal=path="/etc/kubernetes/cloud.conf" -n kube-system
 
+#ARO Cluster permission
+oc create clusterrole azure-secret-reader --verb=create,get --resource=secrets
+oc adm policy add-cluster-role-to-user azure-secret-reader system:serviceaccount:kube-system:persistent-volume-binder
+
 export driver_version=v1.12.0
 echo "Driver version " $driver_version
 ./install-driver.sh $driver_version
