6bd58ce4b9c3eb063e01def1d466409b9fb2dbb5
shajeena <shajsyed@in.ibm.com>
2024-01-16 20:54:26 +0530

    ARO Changes

--- init.sh
+++ init.sh
@@ -227,7 +227,7 @@ export MAS_CATALOG_VERSION=v8-231228-amd64
 if [[ $CLUSTER_TYPE == "aws" ]]; then
   export CPD_PRIMARY_STORAGE_CLASS="ocs-storagecluster-cephfs"
 elif [[ $CLUSTER_TYPE == "azure" ]]; then
-  export CPD_PRIMARY_STORAGE_CLASS="azurefile-csi"
+  export CPD_PRIMARY_STORAGE_CLASS="azurefile-csi-new"
 fi
 # DB2WH variables
 export CPD_OPERATORS_NAMESPACE="ibm-cpd-operators-${RANDOM_STR}"
