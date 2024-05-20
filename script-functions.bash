11634749f8dc6e3601b617af9d9521d8138737b1
Nataraj <nataraj.s@in.ibm.com>
2024-05-20 13:44:52 +0530

    changes..

--- script-functions.bash
+++ script-functions.bash
@@ -9,8 +9,8 @@ op_versions['MongoDBCommunity']=4.1.9
 op_versions['Db2uCluster']=11.4
 op_versions['kafkas.kafka.strimzi.io']=2.4.9
 op_versions['ocpVersion412']='^4\.([1][2])?(\.[0-9][0-9]+.*)*$'
-op_versions['rosaVersion']='^4\.([1][2])?(\.[0-9]+.*)*$'
-op_versions['rosaVersion']='^4\.([1][4])?(\.[0-9]+.*)*$'
+op_versions['rosaVersion412']='^4\.([1][2])?(\.[0-9]+.*)*$'
+op_versions['rosaVersion414']='^4\.([1][4])?(\.[0-9]+.*)*$'
 op_versions['cpd-platform-operator']=2.0.7
 op_versions['user-data-services-operator']=2.0.6
 op_versions['ibm-cert-manager-operator']=3.19.9
@@ -37,7 +37,9 @@ checkROSA(){
 		log " ROSA Cluster "
 		currentOpenshiftVersion=$(oc get clusterversion | awk  'NR==2 {print $2 }')
 		log " OCP version is $currentOpenshiftVersion"
-		if [[ $currentOpenshiftVersion =~ ${op_versions[rosaVersion]} ]]; then
+		if [[ $currentOpenshiftVersion =~ ${op_versions[rosaVersion412]} ]]; then
+    		log " ROSA Cluster Supported Version"
+    	elif [[ $currentOpenshiftVersion =~ ${op_versions[rosaVersion414]} ]]; then
     		log " ROSA Cluster Supported Version"
   		else
     		log " Unsupported ROSA version $currentOpenshiftVersion. Supported ROSA versions are 4.12.x and 4.14.x"
