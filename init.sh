cb7bd68115c494beeb7920c0de768038db4d5e58
shajeena <shajsyed@in.ibm.com>
2024-06-19 22:05:52 +0530

    [minor] AROdb2

--- init.sh
+++ init.sh
@@ -245,7 +245,7 @@ export CPD_INSTANCE_NAMESPACE="ibm-cpd-${RANDOM_STR}"
 #CPD_SERVICES_NAMESPACE is used in roles - cp4d, cp4dv3_install, cp4dv3_install_services and suite_dns
 export CPD_SERVICES_NAMESPACE="cpd-services-${RANDOM_STR}"
 export DB2WH_INSTANCE_NAME="db2wh-cpd-${RANDOM_STR}"
-export DB2WH_VERSION="11.5.8.0-CN1"
+#export DB2WH_VERSION="11.5.8.0-CN1"
 export DB2_META_STORAGE_CLASS=$CPD_PRIMARY_STORAGE_CLASS
 export DB2_DATA_STORAGE_CLASS=$CPD_PRIMARY_STORAGE_CLASS
 export DB2_BACKUP_STORAGE_CLASS=$CPD_PRIMARY_STORAGE_CLASS
@@ -254,7 +254,7 @@ export DB2_TEMP_STORAGE_CLASS=$CPD_PRIMARY_STORAGE_CLASS
 export CPD_SERVICE_NAME="db2wh"
 
 export DB2_INSTANCE_NAME=db2wh-db01
-export DB2_VERSION=11.5.7.0-cn2
+#export DB2_VERSION=11.5.7.0-cn2
 export ENTITLEMENT_KEY=$SLS_ENTITLEMENT_KEY
 # not reqd its hardcoded as db2_namespace: db2u
 export DB2WH_NAMESPACE="cpd-services-${RANDOM_STR}"
