a3cf3cacafc8284aecc867ecd2d821255a87942b
shajeena <shajsyed@in.ibm.com>
2024-06-25 15:21:26 +0530

    [minor] AROdb2

--- azure/bootnode-image/prepare-bootnode-image.sh
+++ azure/bootnode-image/prepare-bootnode-image.sh
@@ -44,11 +44,11 @@ dnf update -y
 #dnf update python39 python-unversioned-command -y
 
 
-ln -s --force /usr/bin/python3.9 /usr/bin/python
-ln -s --force /usr/bin/pip3.9 /usr/bin/pip
+#ln -s --force /usr/bin/python3.9 /usr/bin/python
+#ln -s --force /usr/bin/pip3.9 /usr/bin/pip
 
-ln -s --force /usr/bin/python3.9 /usr/bin/python3
-ln -s --force /usr/bin/pip3.9 /usr/bin/pip3
+#ln -s --force /usr/bin/python3.9 /usr/bin/python3
+#ln -s --force /usr/bin/pip3.9 /usr/bin/pip3
 
 #Install openshift-install 4.14.26
 wget "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.14.26/openshift-client-linux.tar.gz"
