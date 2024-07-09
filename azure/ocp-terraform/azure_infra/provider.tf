4361b5e0f117e9ff151ec4f746dfe564e96e110b
shajeena <shajsyed@in.ibm.com>
2024-07-09 19:15:37 +0530

    [minor] provider

--- azure/ocp-terraform/azure_infra/provider.tf
+++ azure/ocp-terraform/azure_infra/provider.tf
@@ -1,8 +1,15 @@
 provider "azurerm" {
-  version         = "= 2.6.0"
   subscription_id = var.azure-subscription-id
   client_id       = var.azure-client-id
   client_secret   = var.azure-client-secret
   tenant_id       = var.azure-tenant-id
   features {}
+}
+terraform {
+  required_providers {
+  azurerm = {
+  source  = "hashicorp/azurerm"
+  version = "3.0.2"
+   }
+  }
 }
