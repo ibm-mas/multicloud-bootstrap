4c1fe2951dabdf194e7cbbab7f1e43648352d39d
shajeena <shajsyed@in.ibm.com>
2024-07-24 15:10:35 +0530

    dnf update

--- azure/bootnode-image/prepare-bootnode-image.sh
+++ azure/bootnode-image/prepare-bootnode-image.sh
@@ -32,16 +32,14 @@ dnf -y remove polkit
 #echo "Enable and disable repos to update certs"
 #dnf update -y --disablerepo=* --enablerepo='*microsoft*' rhui-azure-rhel8-eus
 rm /etc/dnf/vars/releasever
-#dnf --disablerepo='*' -y remove 'rhui-azure-rhel9-eus'
-#dnf --config='https://rhelimage.blob.core.windows.net/repositories/rhui-microsoft-azure-rhel9.config' install rhui-azure-rhel9 -y
+dnf --disablerepo='*' -y remove 'rhui-azure-rhel9-eus'
+dnf --config='https://rhelimage.blob.core.windows.net/repositories/rhui-microsoft-azure-rhel9.config' install rhui-azure-rhel9 -y
 python3 --version
 # Update all packages to latest
 dnf clean all
 #dnf update -y
 # yum install python-unversioned-command -y
 ## Install pre-reqs
-subscription-manager register
-insights-client --register
 
 dnf update -y
 dnf install vsftpd bind -y
