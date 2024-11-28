7b0413a7e4d6a1ddf197e0b4a8dc1ab5fe1d3604
Nataraj <nataraj.s@in.ibm.com>
2024-11-28 21:42:11 +0530

    Changes specific to AWS for OCP 4.15.x support..

--- aws/ocp-terraform/mac-prereq-install.sh
+++ aws/ocp-terraform/mac-prereq-install.sh
@@ -19,8 +19,8 @@ chmod +x jq
 mv jq /usr/local/bin
 
 ## Download Openshift CLI and move to /usr/local/bin
-#Install openshift-install 4.15.38
-wget "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.15.38/openshift-client-linux.tar.gz"
+#Install openshift-install 4.15.39
+wget "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.15.39/openshift-client-linux.tar.gz"
 tar -xvf openshift-client-linux.tar.gz
 chmod u+x oc kubectl
 mv -f oc /usr/local/bin
