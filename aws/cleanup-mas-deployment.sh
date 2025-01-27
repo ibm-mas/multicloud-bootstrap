2ce92a5bf0d0e1537c47cad718ada523291af38f
Nataraj <nataraj.s@in.ibm.com>
2025-01-27 12:06:00 +0530

    Updating with GovCloud region entries..

--- aws/cleanup-mas-deployment.sh
+++ aws/cleanup-mas-deployment.sh
@@ -75,7 +75,7 @@ if [[ -z $REGION ]]; then
   echo "ERROR: Parameter 'region-code' not provided"
   usage
 fi
-SUPPORTED_REGIONS="us-east-1;us-east-2;us-west-2;us-gov-west-1;ap-east-1;ap-southeast-1;ap-southeast-2;ap-northeast-1;eu-central-1;ap-south-1;ap-northeast-2;ap-northeast-3;ca-central-1;eu-south-1;eu-west-1;eu-west-2;eu-west-3;eu-north-1;af-south-1;me-south-1;sa-east-1"
+SUPPORTED_REGIONS="us-east-1;us-east-2;us-west-2;us-gov-west-1;us-gov-east-1;ap-east-1;ap-southeast-1;ap-southeast-2;ap-northeast-1;eu-central-1;ap-south-1;ap-northeast-2;ap-northeast-3;ca-central-1;eu-south-1;eu-west-1;eu-west-2;eu-west-3;eu-north-1;af-south-1;me-south-1;sa-east-1"
 if [[ ${SUPPORTED_REGIONS} =~ $REGION ]]; then
   echo "Supported region provided"
 else
